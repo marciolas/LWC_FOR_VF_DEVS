7096792d6624cfcbda8333179da08b31be1bd339
Márcio Las <marciolas@outlook.com.br>
2024-03-05 09:08:45 -0300

    Handle User Actions in JavaScript

---
+++ force-app/main/default/lwc/accountSearch/accountSearch.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class AccountSearch extends LightningElement {
+    numberOfEmployees = null;
+
+    handleChange(event) {
+        this.numberOfEmployees = event.detail.value;
+    }
+    
+    reset() {
+        this.numberOfEmployees = null;
+    }
+}
